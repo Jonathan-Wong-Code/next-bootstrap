b22299ca7ac5cda5a1b45638e040cb693588b242
Jonathan Wong <jonathan.wong@applydigital.co>
2021-01-22 10:21:29 -0500

    Add global reset to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { LocalizationProvider } from '../src/i18n/LocalizationProvider';
 import { withNextRouter } from 'storybook-addon-next-router';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/styles/themes';
+import '../src/styles/globals.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
